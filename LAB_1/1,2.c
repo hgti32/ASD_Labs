f744e7904470d176b65d6d4ed4b5b65116ca7e55
hgti32 <127024694+hgti32@users.noreply.github.com>
2025-10-23 23:32:17 +0300

    Initial commit

---
+++ LAB_1/1,2.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+
+    int num, x, y;
+    int inf = INFINITY;
+
+    printf("Input - x : ");
+    scanf("%d", &num);
+
+    if (num >= -15) {
+        if (num <= 3) {
+            y = 4 * num * 2 + 2;
+            printf("Y = %d \n", y);
+            return 0;
+        }
+    }
+
+    else if (num >= -INFINITY) {
+        if (num <= - 30) {
+            y = 3 * num * 3 / 4 - 5;
+            printf("X = %d \n", y);
+            return 0;
+        }
+    }
+
+    if (num >= 20) {
+        if (num <= inf) {
+            x = 3 * num * 3 / 4 - 5;
+            printf("X = %d \n", x);
+        }
+    }
+
+    else {
+        printf("ERROR!! \n");
+    }
+    return 0;
+}
